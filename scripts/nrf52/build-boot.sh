d96de31ebeb6123c3973b76d26f00363cb3269f9
kung1388 <kung1388@hotmail.com>
2021-03-02 17:22:30 +0800

    Update build-boot.sh

--- scripts/nrf52/build-boot.sh
+++ scripts/nrf52/build-boot.sh
@@ -16,11 +16,11 @@ newt build nrf52_boot
 newt size -v nrf52_boot
 
 arm-none-eabi-objcopy -I binary -O ihex bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.elf.bin bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.elf.hex
-scripts/hex2c.py bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.elf.hex > reloader/src/boards/pinetime/bootloader.h
-make -C reloader BOARD=pinetime
+scripts/hex2c.py bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.elf.hex > reloader/src/boards/p8/bootloader.h
+make -C reloader BOARD=p8
 
 set +x
 echo "Bootloader firmware (elf) : bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.elf"
 echo "Bootloader firmware (bin) : bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.bin"
 echo "Bootloader firmware (hex) : bin/targets/nrf52_boot/app/@mcuboot/boot/mynewt/mynewt.hex"
-echo "Reloader (DFU) : reloader/build-pinetime/reloader-mcuboot.zip"
+echo "Reloader (DFU) : reloader/build-p8/reloader-mcuboot.zip"
